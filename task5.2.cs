aa78e33f58bdf106c4f9d5246a418ff0376f8982
LifeToHell <120266818+LifeToHell@users.noreply.github.com>
2023-01-15 18:42:09 +0400

    Add files via upload

---
+++ task5.2.cs
@@ -0,0 +1,25 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+//[3, 7, 23, 12] -> 19
+
+//[-4, -6, 89, 6] -> 0
+
+Console.Clear();
+Console.WriteLine("Введите размер массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [n];
+int count = 0;
+int sum = 0;
+for(int i = 0; i < array.Length; i++){
+array[i] = new Random(). Next(0,99);
+} 
+Console.WriteLine($"[{string.Join(",", array)}]");
+
+for (int z = 0; z < array.Length; z++)
+if (array[z] % 2 == 1){
+   sum = sum + array[z]; 
+   count++;
+}
+
+Console.WriteLine($"всего {array.Length} чисел, {count} из них не чётные");
+Console.WriteLine("Сумма элементов с нечетными номерами (нумерация начинается с нуля): {0} ", sum);
