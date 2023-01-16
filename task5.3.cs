487df8bc7c4d1bf179f387e7398f9a619213e6ae
LifeToHell <120266818+LifeToHell@users.noreply.github.com>
2023-01-16 20:33:19 +0400

    Add files via upload

---
+++ task5.3.cs
@@ -0,0 +1,19 @@
+﻿//Задача 38: Задайте массив чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+//[3 7 22 2 78] -> 76
+
+
+Console.Clear();
+Console.WriteLine("Введите размер массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+int dif = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 99);
+}
+dif = (array.Max() - array.Min());
+Console.WriteLine($"[{string.Join(",", array)}]");
+Console.WriteLine("Минимальное число массива " + array.Min());
+Console.WriteLine("Максимальное число массива " + array.Max());
+Console.WriteLine("Разница между максимальным и минимальным значением " + dif);
